f36e6ba86ff0c9bb64c681741b9bb7d7d0c93c62
Eric Roginek <eroginek@protonmail.com>
2017-09-19 18:33:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element){
-  return array.unshift(element)
+  var elements = array()
+  elements.unshift(element)
+  return elements
 }
 
 
